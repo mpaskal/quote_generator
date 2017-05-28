4f715387ae43e089f4c69c9ca6b9c99155ed39d4
Marina Paskal <paskalm@rogers.com>
2017-05-28 01:43:59 -0400

    Added background and tweet

--- javascript.js
+++ javascript.js
@@ -1,29 +1,46 @@
 var quotes = [
-    'Classes will dull your mind, destroy the potential for authentic creativity. - John Nash 1', 
-    'If you\'re changing the world, you\'re working on important things. You\'re excited to get up in the morning. - Larry Page 2',
-    'Trust yourself. Create the kind of self that you will be happy to live with all your life. Make the most of yourself by fanning the tiny, inner sparks of possibility into flames of achievement. - Golda Meir 3',
-    'Daydreams are fertile ground for the imagination. While daydreaming rational thought goes out the window, so new perspectives can find their way in. - Linda Naiman 4',
-    'Life is not about finding yourself. Life is about creating yourself. - George Bernard Shaw 5',
-    'It is better to begin in the evening than not at all. - English proverb 6',
-    'Whatever you do, or dream you can, begin it. Boldness has genius and power and magic in it. - Goethe 7'
+    ['Classes will dull your mind, destroy the potential for authentic creativity.', '- John Nash'], 
+    ['If you\'re changing the world, you\'re working on important things. You\'re excited to get up in the morning.', '- Larry Page'],
+    ['Trust yourself. Create the kind of self that you will be happy to live with all your life. Make the most of yourself by fanning the tiny, inner sparks of possibility into flames of achievement.', '- Golda Meir'],
+    ['Daydreams are fertile ground for the imagination. While daydreaming rational thought goes out the window, so new perspectives can find their way in.', '- Linda Naiman'],
+    ['Life is not about finding yourself. Life is about creating yourself.', '- George Bernard Shaw'],
+    ['It is better to begin in the evening than not at all.', '- English proverb'],
+    ['Whatever you do, or dream you can, begin it. Boldness has genius and power and magic in it.', '- Goethe'],
+    ['Tetris taught me that when you try to fit in you’ll disappear.', 'Somebody']
 ];
 
 var randomNumber;
-var quotesNew = [];
+var quotesNew = new Array();
 var message = "Sorry, there is nothing to show";
+var quote = "";
+var author = "";
 
 function newQuote(){
     if(quotes.length <= 0){
         document.getElementById('quoteDisplay').innerHTML = message;
     } else if(quotes.length > 1){
         randomNumber = Math.floor(Math.random() * quotes.length);
-        document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
-        quotesNew.push(quotes.splice(randomNumber,1));          
+        quote = document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber][0];
+        author = document.getElementById('authorDisplay').innerHTML = quotes[randomNumber][1];       
+        quotesNew.push(quotes.splice(randomNumber,1)[0]);         
     } else if(quotes.length === 1){
         randomNumber = Math.floor(Math.random() * quotes.length);
-        document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
-        quotesNew.push(quotes.splice(randomNumber,1));        
+        quote = document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber][0];
+        author = document.getElementById('authorDisplay').innerHTML = quotes[randomNumber][1];
+        quotesNew.push(quotes.splice(randomNumber,1)[0]);      
         quotes = quotesNew;
         quotesNew = [];
     }   
 }
+
+function twitter(){
+    if(quote != "") {
+        var url = 'https://twitter.com/intent/tweet?text=' + quote + ' by ' + author;
+            window.open(url, 'twitter');  
+        
+    } else {  
+        var url = 'https://twitter.com/intent/tweet?text=' + 'No quotes were selected.';
+            window.open(url, 'twitter');  
+    }
+} 
+
